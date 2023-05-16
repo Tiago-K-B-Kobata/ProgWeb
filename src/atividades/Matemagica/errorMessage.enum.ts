b96ad879a93dc3524723151e8cedb3cd4a706a6d
Tiago-K-B-Kobata <tiago_kobata@hotmail.com>
2023-05-16 15:28:28 -0300

    Matemagica

---
+++ src/atividades/Matemagica/errorMessage.enum.ts
@@ -0,0 +1,4 @@
+export enum ErrorMessages {
+    INVALID_NUMBER_RANGE = "A quantidade de elementos aleatórios é superior ao número de elementos na lista!",
+    INVALID_TYPE = "Tipo da incerção invalida"
+}
