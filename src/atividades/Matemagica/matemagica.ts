f30dc8106ce30ee5a8886152b40c215aa9b84d1d
Tiago-K-B-Kobata <tiago_kobata@hotmail.com>
2023-05-16 15:28:42 -0300

    Matemagica

--- src/atividades/Matemagica/matemagica.ts
+++ src/atividades/Matemagica/matemagica.ts
@@ -1,3 +1,5 @@
+import { ErrorMessages } from "./errorMessage.enum"
+
 class matemagica {
     x:number
     y:number
@@ -6,11 +8,37 @@ class matemagica {
         return this.x + this.y
     }
 
+    private subXY(){
+        return this.x - this.y
+    }
+
+    private subYX(){
+        return this.y - this.x
+    }
+
+    private divXY(){
+        return this.x / this.y
+    }
+
+    private divYX(){
+        return this.y / this.x
+    }
+
+    private mult(){
+        return this.x * this.y
+    }
+
     public setX(x) {
+        if(typeof x !== 'number'){
+            return ErrorMessages.INVALID_TYPE
+        }
         this.x = x
     }
 
     public setY(y){
+        if(typeof y !== 'number'){
+            return ErrorMessages.INVALID_TYPE
+        }
         this.y = y
     }
 
@@ -24,6 +52,26 @@ class matemagica {
 
     public getSum(){
         return this.sum()
+    }
+
+    public getSubXY(){
+        return this.subXY()
+    }
+    
+    public getSubYX(){
+        return this.subYX()
+    }
+
+    public getDivXY(){
+        return this.divXY()
+    }
+    
+    public getDivYX(){
+        return this.divYX()
+    }
+
+    public getMult(){
+        return this.mult()
     }
 
 }
